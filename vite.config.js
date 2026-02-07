4618c68bf6830a90c928a70baecd9291a14885d8
TechStackIns <techstackinsights@gmail.com>
2026-02-07 12:14:21 +0530

    pre deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/pankaj-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
